1449257fcb38273d652fe626f1ea46416acca04e
beansdrawer@naver.com <eau5238@gmail.com>
2026-01-21 20:05:20 +0900

    if 문

--- 2025-12-09/main.py
+++ 2025-12-09/main.py
@@ -1,8 +1,13 @@
-num1 = 15
-num = 25
- 합계산
-total = numl + num2
-print ("합:", total)
+# 변수를 선언해서 정수 a,b를 입력받고, 다음 조건에 따라 만족하면 1을, 만족하지 않는다면 0을 각 줄에 출력합니다
+# input() 한줄로 입력
+# split() 입력한 문자열 공백 기준으로 나눔
+# map(int,..)"",""를 정수로 (int)로 바뀜
 
-average = total/2
-print("평균:", average)
+a,b = map(int,input().split())
+
+print(int(a >= b))
+print(int(a > b ))
+print(int(a <= b ))
+print(int(a < b ))
+print(int(a == b ))
+print(int(a != b ))
---
+++ 2026-01-21/main.py
@@ -0,0 +1,8 @@
+num1 = 15
+num = 25
+ 합계산
+total = numl + num2
+print ("합:", total)
+
+average = total/2
+print("평균:", average)
